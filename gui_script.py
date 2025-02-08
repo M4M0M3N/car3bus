08f9ad9d5441eadfab07b286faed2d636a63e2de
M4M0M3N <79576670+M4M0M3N@users.noreply.github.com>
2025-02-08 23:19:19 +0100

    Aggiunta pagina 2
    Aggiunta pagina 2 con lancetta rossa e sfondo nero
    Aggiunta progress bar che cambia colore/valore in base alla posizione acceleratore
    Aggiunto file font "digital display" per visualizzare posizione acceleratore

--- gui_script.py
+++ gui_script.py
@@ -1,12 +1,11 @@
 import os
-from time import sleep
 
 from PyQt5.QtWidgets import QGraphicsPixmapItem
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from gui.mainwindow import Ui_MainWindow
 from PyQt5.QtGui import QPixmap, QTransform
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, m):
@@ -28,45 +27,73 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.pushButton.clicked.connect(self.switch_page)
 
-        self.scene = QtWidgets.QGraphicsScene()
-        self.contenitore_lancette.setScene(self.scene)  # Associa la scena al pedaleacc_lancetta_2
+        #aggiungi scena e adattala alle dimensioni del graphicsView
+        self.scene_page_1 = QtWidgets.QGraphicsScene()
+        self.scene_page_1.setSceneRect(QRectF(0, 0, self.contenitore_lancette.width()-10, self.contenitore_lancette.height()-10))
+        self.contenitore_lancette.setScene(self.scene_page_1)  # Associa la scena al pedaleacc_lancetta_2
+
+    #impostare page 1
 
         # Aggiungi l'immagine alla scena
-        pixmap = QPixmap('lancetta.png')  # Sostituisci con il percorso della tua immagine
+        pixmap_1 = QPixmap('lancetta.png')  # Sostituisci con il percorso della tua immagine
 
-        # Ridimensiona il pixmap alla dimensione desiderata
-        pixmap = pixmap.scaled(100, 10)
+        # Ridimensiona il pimax_1 alla dimensione desiderata
+        ridimensiona = pixmap_1.width()/100
+        pixmap_1 = pixmap_1.scaled(pixmap_1.width()/ridimensiona, pixmap_1.height()/ridimensiona)
 
-        self.velocita_lancetta = QGraphicsPixmapItem(pixmap)
-        self.girimotore_lancetta = QGraphicsPixmapItem(pixmap)
-        self.pedaleacc_lancetta = QGraphicsPixmapItem(pixmap)
-        self.temperatura_lancetta = QGraphicsPixmapItem(pixmap)
+        self.velocita_lancetta = QGraphicsPixmapItem(pixmap_1)
+        self.girimotore_lancetta = QGraphicsPixmapItem(pixmap_1)
+        self.pedaleacc_lancetta = QGraphicsPixmapItem(pixmap_1)
+        self.temperatura_lancetta = QGraphicsPixmapItem(pixmap_1)
 
-        self.scene.addItem(self.velocita_lancetta)
-        self.scene.addItem(self.girimotore_lancetta)
-        self.scene.addItem(self.pedaleacc_lancetta)
-        self.scene.addItem(self.temperatura_lancetta)
+        self.scene_page_1.addItem(self.velocita_lancetta)
+        self.scene_page_1.addItem(self.girimotore_lancetta)
+        self.scene_page_1.addItem(self.pedaleacc_lancetta)
+        self.scene_page_1.addItem(self.temperatura_lancetta)
 
-        x,y = pixmap.width() / 2, pixmap.height() / 2
+        x,y = pixmap_1.width() / 2, pixmap_1.height() / 2
 
         self.velocita_lancetta.setTransformOriginPoint(x,y)
         self.girimotore_lancetta.setTransformOriginPoint(x,y)
         self.pedaleacc_lancetta.setTransformOriginPoint(x,y)
         self.temperatura_lancetta.setTransformOriginPoint(x,y)
 
+        x, spazio = 20, 58
 
-        x, spazio = 0, 58
+        self.velocita_lancetta.setPos(x, spazio)
+        self.girimotore_lancetta.setPos(x, spazio*3)
+        self.pedaleacc_lancetta.setPos(x, spazio*5)
+        self.temperatura_lancetta.setPos(x, spazio*7)
 
-        self.velocita_lancetta.setPos(x, 0)
-        self.girimotore_lancetta.setPos(x, 100)
-        self.pedaleacc_lancetta.setPos(x, 150)
-        self.temperatura_lancetta.setPos(x, 200)
+    #impostare page 2
 
-        #self.item.setTransform(transform)
+        self.scene_page_2 = QtWidgets.QGraphicsScene()
+        self.scene_page_2.setSceneRect(QRectF(0, 0, self.contenitore_page_2.width() - 10, self.contenitore_page_2.height() - 10))
+        self.contenitore_page_2.setScene(self.scene_page_2)
+
+        # Aggiungi l'immagine alla scena_2
+        pixmap_2_s = QPixmap('sfondo_2')
+        pixmap_2_l = QPixmap('lancetta_2')
+
+        # Ridimensiona il pimax_1 alla dimensione desiderata
+        ridimensiona_s = pixmap_2_s.width() / self.contenitore_page_2.width()
+        ridimensiona_l = pixmap_2_l.width() / self.contenitore_page_2.width()
+
+        pixmap_2_s = pixmap_2_s.scaled(pixmap_2_s.width() / ridimensiona_s, pixmap_2_s.height() / ridimensiona_s)
+        pixmap_2_l = pixmap_2_l.scaled(pixmap_2_l.width() / ridimensiona_l, pixmap_2_l.height() / ridimensiona_l)
+
+        self.giri_motore_2_s = QGraphicsPixmapItem(pixmap_2_s)
+        self.giri_motore_2_l = QGraphicsPixmapItem(pixmap_2_l)
+
+        self.giri_motore_2_l.setTransformOriginPoint(pixmap_2_l.width() / 2, pixmap_2_l.height() / 2)
+
+        self.scene_page_2.addItem(self.giri_motore_2_s)
+        self.scene_page_2.addItem(self.giri_motore_2_l)
+
+        self.pedaleacc_bar.setOrientation(Qt.Vertical)
 
 
-
-        # Timer per aggiornare le lancette e i testi
+    # Timer per aggiornare le lancette e i testi
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.update_ui)
         self.timer.start(50)  # Aggiorna ogni mezzo secondo
@@ -75,10 +102,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Cambia pagina del QStackedWidget
         current_index = self.stackedWidget.currentIndex()
 
-        if current_index == 1:
-            current_index = -1
-
-        self.stackedWidget.setCurrentIndex(current_index + 1)
+        if current_index == self.stackedWidget.count()-1:
+            self.stackedWidget.setCurrentIndex(0)
+        else:
+            self.stackedWidget.setCurrentIndex(current_index + 1)
 
 
     def update_ui(self):
@@ -93,12 +120,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         try:
             angle_girimotore = self.m.giri_motore / 6000 * 180
             self.girimotore_lancetta.setRotation(angle_girimotore)
+
+            angle_girimotore = self.m.giri_motore / 9000 * 270
+            self.giri_motore_2_l.setRotation(angle_girimotore)
         except:
             pass
 
         try:
             angle_pedaleacc = self.m.pedale_acceleratore / 100 * 180
             self.pedaleacc_lancetta.setRotation(angle_pedaleacc)
+            self.pedaleacc_bar.setValue(self.m.pedale_acceleratore)
+
+            self.update_progress_bar_color()
+
         except:
             pass
 
@@ -112,12 +146,47 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         try:
             # Aggiorna i testi con i valori attuali
             self.velocita_text.setText(f"Velocità: {self.m.velocita} km/h")
+            self.velocita_text_2.setText(str(self.m.velocita))
             self.girimotore_text.setText(f"Giri Motore: {self.m.giri_motore} RPM")
             self.pedaleacc_text.setText(f"Pedale: {self.m.pedale_acceleratore} %")
             self.temperatura_text.setText(f"Temperatura: {self.m.temperatura_motore} °C")
 
         except:
             pass
+
+    def update_progress_bar_color(self):
+        value = self.pedaleacc_bar.value()
+
+        if value <= 40:
+            # Verde
+            red = 0
+            green = 255
+        elif 41 <= value <= 50:
+            # Da verde a giallo
+            red = int(((value - 40) / 10) * 255)
+            green = 255
+        elif 51 <= value <= 70:
+            # Giallo
+            red = 255
+            green = 255
+        elif 71 <= value <= 80:
+            # Da giallo a rosso
+            red = 255
+            green = int(255 - ((value - 70) / 10) * 255)
+        else:
+            # Rosso
+            red = 255
+            green = 0
+
+        color = f"rgb({red}, {green}, 0)"
+
+        # Aggiorna il foglio di stile della barra di progresso
+        self.pedaleacc_bar.setStyleSheet(f"""
+            QProgressBar::chunk {{
+                background-color: {color};
+            }}
+        """)
+
 
 def run(m):
     import warnings
