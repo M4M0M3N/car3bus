41bc21c4d6bdc68c188ebf7c4ba9141815b3aa17
M4M0M3N <matty98m@hotmail.it>
2025-08-20 10:32:11 +0200

    Eliminati elementi pesanti per un raspberry pi3a+

--- gui_script.py
+++ gui_script.py
@@ -1,9 +1,9 @@
 from PyQt5 import QtWidgets, QtCore
 from gui.dashboard_testuale_1 import DashboardTestuale1
 from gui.dashboard_racing_1 import DashboardRacing1
-from gui.dashboard_racing_2 import DashboardRacing2
+#from gui.dashboard_racing_2 import DashboardRacing2        #rallenta un sacco l'elaborazione su un raspberry pi3a
 from gui.dashboard_lancette_1 import DashboardLancette1
-from gui.dashboard_jdm_1 import DashboardJDM1
+#from gui.dashboard_jdm_1 import DashboardJDM1              #brutto da vedere
 from gui.dashboard_grafico_1 import DashboardGrafico1
 
 
@@ -26,19 +26,19 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.testo_1 = DashboardTestuale1(m)
         self.racing_1 = DashboardRacing1(m)
-        self.racing_2 = DashboardRacing2(m)
+        #self.racing_2 = DashboardRacing2(m)
         self.lancette_1 = DashboardLancette1(m)
-        self.jdm_1 = DashboardJDM1(m)
+        #self.jdm_1 = DashboardJDM1(m)
         self.grafico_1 = DashboardGrafico1(m)
 
         self.stacked.addWidget(self.testo_1)
         self.stacked.addWidget(self.racing_1)
-        self.stacked.addWidget(self.racing_2)
+        #self.stacked.addWidget(self.racing_2)
         self.stacked.addWidget(self.lancette_1)
-        self.stacked.addWidget(self.jdm_1)
+        #self.stacked.addWidget(self.jdm_1)
         self.stacked.addWidget(self.grafico_1)
 
-        self.stacked.setCurrentWidget(self.racing_2)
+        self.stacked.setCurrentWidget(self.lancette_1)
 
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update_all)
@@ -53,21 +53,20 @@ class MainWindow(QtWidgets.QMainWindow):
             i = (self.stacked.currentIndex() - 1) % self.stacked.count()
             self.stacked.setCurrentIndex(i)
         
-        # Gestione cambio cartella frame se attiva racing_2
+        '''# Gestione cambio cartella frame se attiva racing_2
         elif event.key() == QtCore.Qt.Key_Up:
             if self.stacked.currentWidget() == self.racing_2:
                 self.racing_2.cambia_cartella(+1)
         elif event.key() == QtCore.Qt.Key_Down:
             if self.stacked.currentWidget() == self.racing_2:
-                self.racing_2.cambia_cartella(-1)
+                self.racing_2.cambia_cartella(-1)'''
 
         self.setWindowTitle(f"{type(self.stacked.currentWidget()).__name__} - Mazda Dashboard")
 
     def update_all(self):
-        # Aggiorna sempre la dashboard grafica
+        # Aggiorna sempre la dashboard coi grafici
         self.grafico_1.aggiorna()
         
         # Aggiorna anche la GUI attiva, se diversa da grafico_1
-        current_widget = self.stacked.currentWidget()
-        if current_widget is not self.grafico_1:
+        if self.stacked.currentWidget() is not self.grafico_1:
             current_widget.aggiorna()
